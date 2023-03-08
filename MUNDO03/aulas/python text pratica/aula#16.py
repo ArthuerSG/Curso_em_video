03cbb312664feebf152cdc21e118e41c4c4af38b
Tuco_1001 <arthursgrasso@gmail.com>
2023-03-08 17:38:14 -0300

    d

---
+++ MUNDO03/aulas/python text pratica/aula#16.py
@@ -0,0 +1,40 @@
+# lanche = ("Hambúrguer", 'Suco', 'Pizza', 'Pudim', 'Batata Frita')
+# Tuplas são imutáveis
+
+# print(lanche)
+# print(lanche[-2])
+# print(lanche[1:3])
+# print(lanche[0])
+# print(lanche[:3])
+
+# print(sorted(lanche))
+
+#01
+# for comida in lanche:
+#     print(f'Eu vou comer {comida}')
+
+#02
+# for cont in range(0, len(lanche)):
+#     print(f'Eu vou comer {lanche[cont]} na posição {cont}')
+
+#03
+# for pos, comida in enumerate(lanche):
+#     print(f'Eu vou comer {comida} na posição {pos}')
+
+# print('Comi pra caramba!')
+# print(len(lanche))
+
+
+# a = (2, 5, 4)
+# b = (5, 8, 1, 2)
+# c = a + b
+# print(c)
+# nprint(c.index(4))
+
+pessoa =('Gustavo', 39, 'M', 99.88)
+del(pessoa)
+print(pessoa)
+
+
+
+
